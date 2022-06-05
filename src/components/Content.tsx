5167a4deb18088a18a82ff2b9993d0f762c9298a
Bruno Santos <brunosantosfarias2@gmail.com>
2022-06-05 02:30:45 -0300

    update - desafio Otimizando a aplicação

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,4 +1,5 @@
-import '../styles/content.scss';
+import { memo } from "react";
+import "../styles/content.scss";
 
 import { MovieCard } from "./MovieCard";
 
@@ -15,25 +16,35 @@ interface ContentProps {
   }>;
   selectedGenre: {
     id: number;
-    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
     title: string;
   };
 }
 
-export function Content(props: ContentProps) {
+function ContentComponent(props: ContentProps) {
   return (
     <div className="container">
       <header>
-        <span className="category">Categoria:<span> {props.selectedGenre.title}</span></span>
+        <span className="category">
+          Categoria:<span> {props.selectedGenre.title}</span>
+        </span>
       </header>
 
       <main>
         <div className="movies-list">
-          {props.movies.map(movie => (
-            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          {props.movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
           ))}
         </div>
       </main>
     </div>
-  )
+  );
 }
+
+export const Content = memo(ContentComponent);
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,28 +1,31 @@
-import '../styles/sidebar.scss'
+import { memo, useCallback } from "react";
+import "../styles/sidebar.scss";
 
 import { Button } from "./Button";
 
 interface SideBarProps {
   genres: Array<{
     id: number;
-    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
     title: string;
   }>;
   selectedGenreId: number;
   setSelectedGenreId: (id: number) => void;
 }
 
-export function SideBar(props: SideBarProps) {
-  function handleClickButton(id: number) {
+function SideBarComponent(props: SideBarProps) {
+  const handleClickButton = useCallback((id: number) => {
     props.setSelectedGenreId(id);
-  }
-  
+  }, []);
+
   return (
     <nav className="sidebar">
-      <span>Watch<p>Me</p></span>
+      <span>
+        Watch<p>Me</p>
+      </span>
 
       <div className="buttons-container">
-        {props.genres.map(genre => (
+        {props.genres.map((genre) => (
           <Button
             key={String(genre.id)}
             title={genre.title}
@@ -33,5 +36,7 @@ export function SideBar(props: SideBarProps) {
         ))}
       </div>
     </nav>
-  )
+  );
 }
+
+export const SideBar = memo(SideBarComponent);
